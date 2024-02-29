1c070a51076a0e3c3219a9e0fa961a369dbf8ca7
Qcc <qiancc4@gmail.com>
2024-02-28 22:06:53 -0800

    update click counter in Board component

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,14 +1,16 @@
 import "./board.css";
 import Cell from "./cell";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { ClickContext } from "../clickContextProvider";
 export default function Board() {
   const { clickStates } = useContext(ClickContext);
+  const clickedNumber = useMemo(
+    () => clickStates.filter((item) => item === true).length,
+    [clickStates]
+  );
   return (
     <>
-      <div className="counter">
-        Counter: {clickStates.filter((item) => item === true).length}
-      </div>
+      <div className="counter">Counter: {clickedNumber}</div>
       <div className="board_row">
         <Cell id={0} />
         <Cell id={1} />
